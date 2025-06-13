264a8d2a7d35e819cb417f162592324b6ce35ed0
PGeogre <maowu5263@gmail.com>
2025-06-13 15:08:13 +0800

    test how to commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import torch
+import os
+import sys
+import pandas
+import numpy as np
+
+a = torch.tensor([1, 2, 3])
+b = torch.tensor([4, 5, 6])
+c = torch.tensor([7, 8, 9])
+d = torch.tensor([10, 11, 12])
+e = torch.tensor([13, 14, 15])
---
+++ test2.py
@@ -0,0 +1,8 @@
+import torch
+import os
+import sys
+import pandas
+import numpy as np
+
+a = torch.tensor([1, 2, 3])
+b = torch.tensor([4, 5, 6])
